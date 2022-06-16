c3314e5268e8868cf98d7a481f3175c9e86396d6
Erfan <erfammioi@gmail.com>
2022-06-16 11:51:15 +0430

    add favicon and create header and footer

---
+++ resources/main_ui.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<title>QUERA</title>
+<link rel="icon" type="image/x-icon" href="../public/images/apple-touch-icon.2-2887f2aad086.png">
+<meta name="apple-mobile-web-app-title" content="Quera">
+<meta name="application-name" content="Quera">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="msapplication-config" content="/static/images/logo/favicon/browserconfig.2-0049c1f5e338.xml">
+<meta name="theme-color" content="#09c">
+<body>
+<?php include 'header.php';?>
+<?php include 'footer.php';?>
+</body>
+</html>
