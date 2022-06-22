c9a6f5f30c3bb2072c2658421e321da6c119a58c
Erfan <erfammioi@gmail.com>
2022-06-22 10:35:29 +0430

    add contest page

---
+++ resources/contest.php
@@ -0,0 +1,78 @@
+<?php
+$_GET['data']='  <main role="main" class="container ">
+      <div class="d-flex align-items-center p-3 my-3 text-white-50 bg-purple rounded box-shadow">
+        <img class="mr-3" src="../public/images/contest.png" alt="" width="48" height="48">
+        <div class="lh-100">
+          <h6 class="mb-0 text-white lh-100">Bootstrap</h6>
+          <small>Since 2011</small>
+        </div>
+      </div>
+
+      <div class="my-3 p-3 bg-white rounded box-shadow">
+        <h2 class="border-bottom border-gray pb-2 mb-0">مسابقات</h2>
+        <br>
+        <br>
+         <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <h4>مسابقه الگوریتمی</h4>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+            <p><a class="btn btn-secondary" href="#" role="button">جزییات &raquo;</a></p>
+          </div>
+          <div class="col-md-4">
+            <h4>مسابقه کارآموزی دیوار</h4>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+            <p><a class="btn btn-secondary" href="#" role="button">جزییات &raquo;</a></p>
+          </div>
+          <div class="col-md-4">
+            <h4>مسابقه کارآموزی اسنپ</h4>
+            <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+            <p><a class="btn btn-secondary" href="#" role="button">جزییات &raquo;</a></p>
+          </div>
+        </div>
+        <small class="d-block text-right mt-3">
+          <a class="btn btn-primary" href="#" role="button">همه مسابقات</a>
+        </small>
+      </div>
+
+      <div class="my-3 p-3 bg-white rounded box-shadow">
+        <h6 class="border-bottom border-gray pb-2 mb-0">برترین ها</h6>
+        <div class="media text-muted pt-3">
+          <img data-src="../public/images/contest.png" alt="" class="mr-2 rounded">
+          <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+            <div class="d-flex justify-content-between align-items-center w-100">
+              <strong class="text-gray-dark">جواد اطاهریان</strong>
+              <a class="btn btn-info" href="#" role="button">مشاهده</a>
+            </div>
+            <span class="d-block"></span>
+          </div>
+        </div>
+        <div class="media text-muted pt-3">
+          <img data-src="../public/images/contest.png" alt="" class="mr-2 rounded">
+          <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+            <div class="d-flex justify-content-between align-items-center w-100">
+              <strong class="text-gray-dark">مهدی خوبی</strong>
+              <a class="btn btn-info" href="#" role="button">مشاهده</a>
+            </div>
+            <span class="d-block"></span>
+          </div>
+        </div>
+        <div class="media text-muted pt-3">
+          <img data-src="../public/images/contest.png" alt="" class="mr-2 rounded">
+          <div class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+            <div class="d-flex justify-content-between align-items-center w-100">
+              <strong class="text-gray-dark">عرفان مومنی</strong>
+              <a class="btn btn-info" href="#" role="button">مشاهده</a>
+            </div>
+            <span class="d-block"></span>
+          </div>
+        </div>
+        <small class="d-block text-right mt-3">
+        <br>
+        <br>
+          <a href="#">مشاهده همه</a>
+        </small>
+      </div>
+    </main>';
+
+include 'main_ui.php';
--- resources/header.php
+++ resources/header.php
@@ -3,7 +3,7 @@
     <a href="main.php"><img src="../public/svgs/logo-quera-heavy.2-1c1287ee3575.svg"></a>
     <a href="#" class="btn btn-info" >سامانه آموزشی</a>
     <a href="#" class="btn btn-info">بانک سوالات</a>
-    <a href="#" class="btn btn-info">مسابقات</a>
+    <a href="contest.php" class="btn btn-info">مسابقات</a>
     <a href="#" class="btn btn-info">مگنت(استخدام)</a>
 </div>
 
